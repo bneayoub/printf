f9a877a43c85cd06b14a94dcd0f68483008c954e
bneayoub <bne.ayoub@gmail.com>
2023-04-22 21:00:34 +0000

    handle leading zeros problem

--- handle_binary.c
+++ handle_binary.c
@@ -3,21 +3,29 @@
  * handle_binary - Handles the 'b' specifier
  * @args: The argument list to get the integer from
  * Return: number of chars printed
-*/
+ */
 int handle_binary(va_list args)
 {
-	unsigned int n = va_arg(args, unsigned int);
-	int count = 0;
-	unsigned int mask = 1u << (sizeof(unsigned int) * 8 - 1);
+    unsigned int n = va_arg(args, unsigned int);
+    int count = 0;
+    unsigned int mask = 1u << 31;
 
-	while (mask > 0)
-	{
-		if ((n & mask) == 0)
-			count += _putchar('0');
-		else
-			count += _putchar('1');
+    while ((mask & n) == 0 && mask != 0) {
+        mask >>= 1;
+    }
 
-		mask >>= 1;
-	}
-	return (count);
+    while (mask != 0) {
+        if ((n & mask) == 0) {
+            count += _putchar('0');
+        } else {
+            count += _putchar('1');
+        }
+        mask >>= 1;
+    }
+
+    if (count == 0) {
+        count += _putchar('0');
+    }
+
+    return count;
 }
