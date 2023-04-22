0c47d5860c12291fc42910c331d389c6dcf05bdc
bneayoub <bne.ayoub@gmail.com>
2023-04-22 16:32:26 +0000

    added string handler

---
+++ handle_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * handle_string - Handles the 's' specifier for _printf
+ * @args: The argument list to get the string from
+ * Return: The number of characters printed
+ */
+int handle_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+	int count = 0, i;
+
+	if (!str)
+	{
+		str = "(nil)";
+	}
+
+	for (i = 0; str[i]; i++)
+	{
+		count += _putchar(str[i]);
+	}
+
+	return (count);
+}
