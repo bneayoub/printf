3124c60cf6465f934bd9452f5d0357160e6506ff
bneayoub <bne.ayoub@gmail.com>
2023-04-22 10:52:21 +0000

    main.h include libs

--- main.h
+++ main.h
@@ -1,4 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
 
 int _putchar(char s);
 int _printf(const char *format, ...);
+
+#pragma endregion
