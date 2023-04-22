eb56c7899dc924ab9ec9fb5fbf7b1d1c7995681a
Vhelaa1 <halabouzidi@icloud.com>
2023-04-22 18:29:08 +0000

    debugging

--- handle_string.c
+++ handle_string.c
@@ -7,18 +7,42 @@
  */
 int handle_string(va_list args)
 {
-	char *str = va_arg(args, char *);
+	unsigned char *str = va_arg(args, unsigned char *);
 	int count = 0, i;
 
 	if (!str)
-	{
-		str = "(null)";
-	}
-
+		str = (unsigned char *)"(null)";
 	for (i = 0; str[i]; i++)
 	{
-		count += _putchar(str[i]);
+		if (str[i] > 127)
+		{
+			count += _putchar('\\');
+			count += _putchar('x');
+			count += _putchar("0123456789ABCDEF"[str[i] / 16]);
+			count += _putchar("0123456789ABCDEF"[str[i] % 16]);
+		}
+		else if (str[i] == '\\')
+		{
+			i++;
+			switch (str[i])
+			{
+			case 'n':
+				count += _putchar('\n');
+				break;
+			case 't':
+				count += _putchar('\t');
+				break;
+			case '%':
+				count += _putchar('%');
+				break;
+			default:
+				count += _putchar('\\');
+				count += _putchar(str[i]);
+				break;
+			}
+		}
+		else
+			count += _putchar(str[i]);
 	}
-
 	return (count);
 }
