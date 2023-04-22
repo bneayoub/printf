fd590e64de85c218cf599e1b43258ed747b5e00a
Vhelaa1 <halabouzidi@icloud.com>
2023-04-22 20:29:51 +0000

    test case 3

---
+++ test/2-main.c
@@ -0,0 +1,18 @@
+#include "../main.h"
+
+int main(void)
+{
+    int num1 = 45, num2 = -100, num3 = 0;
+    unsigned int num4 = 12345;
+
+    _printf("num1 in binary: %b\n", num1);
+    _printf("num2 in binary: %b\n", num2);
+    _printf("num3 in binary: %b\n", num3);
+    _printf("num4 in binary: %b\n", num4);
+	_putchar('\n');
+	_printf("num1 in binary: %b\n", num1);
+    _printf("num2 in binary: %b\n", num2);
+    _printf("num3 in binary: %b\n", num3);
+    _printf("num4 in binary: %b\n", num4);
+    return (0);
+}
