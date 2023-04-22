793b026fb60146874bb8b310e8c07af77edd5c30
bneayoub <bne.ayoub@gmail.com>
2023-04-22 16:32:16 +0000

    added char handler

---
+++ handle_char.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * handle_char - handles the case c specifier
+ * @args: the argument list to get the char from
+ * Return: the number of chars printed
+ */
+int handle_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	return (_putchar(c));
+}
