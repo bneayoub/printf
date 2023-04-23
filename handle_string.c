cb8fe2e4991b179729a1574822199b1af276a4b1
bneayoub <bne.ayoub@gmail.com>
2023-04-23 11:58:05 +0100

    debugging string handler

--- handle_string.c
+++ handle_string.c
@@ -7,42 +7,15 @@
  */
 int handle_string(va_list args)
 {
-	unsigned char *str = va_arg(args, unsigned char *);
+	char *str = va_arg(args, char *);
 	int count = 0, i;
 
-	if (!str)
-		str = (unsigned char *)"(null)";
-	for (i = 0; str[i]; i++)
+	if (str)
 	{
-		if (str[i] > 127)
+		for (i = 0; str[i]; i++)
 		{
-			count += _putchar('\\');
-			count += _putchar('x');
-			count += _putchar("0123456789ABCDEF"[str[i] / 16]);
-			count += _putchar("0123456789ABCDEF"[str[i] % 16]);
-		}
-		else if (str[i] == '\\')
-		{
-			i++;
-			switch (str[i])
-			{
-			case 'n':
-				count += _putchar('\n');
-				break;
-			case 't':
-				count += _putchar('\t');
-				break;
-			case '%':
-				count += _putchar('%');
-				break;
-			default:
-				count += _putchar('\\');
-				count += _putchar(str[i]);
-				break;
-			}
-		}
-		else
 			count += _putchar(str[i]);
+		}
 	}
 	return (count);
 }
