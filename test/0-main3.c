cb275bafa01ba911c536568b97b5cfdfd40fd27c
bneayoub <bne.ayoub@gmail.com>
2023-04-23 17:00:17 +0100

    more test cases for debugging

---
+++ test/0-main3.c
@@ -0,0 +1,12 @@
+#include "../main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_printf("Hala's %c, IQ is growing.\n", 72);
+	printf("Hala's %c, IQ is growing.\n", 72);
+	return (0);
+}
