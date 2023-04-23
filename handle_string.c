b2d57143ecc2820b49603a921fae4d9050d6243a
bneayoub <bne.ayoub@gmail.com>
2023-04-23 12:26:36 +0100

    debugging string handler

--- handle_string.c
+++ handle_string.c
@@ -8,16 +8,38 @@
 int handle_string(va_list args)
 {
 	char *str = va_arg(args, char *);
-	int count = 0;
+	int count = 0, i = 0;
 
 	if (str == NULL)
 		str = "(null)";
 
-	while (*str)
+	while (str[i])
 	{
-		count += _putchar(*str);
-		str++;
+		if (str[i] == '\\')
+		{
+			i++;
+			switch (str[i])
+			{
+			case 'n':
+				count += _putchar('\n');
+				break;
+			case 't':
+				count += _putchar('\t');
+				break;
+			case '%':
+				count += _putchar('%');
+				break;
+			default:
+				count += _putchar('\\');
+				count += _putchar(str[i]);
+				break;
+			}
+		}
+		else
+		{
+			count += _putchar(str[i]);
+		}
+		i++;
 	}
-
 	return (count);
 }
