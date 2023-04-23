3e32c43371e3639dce41ac305a4385aa707a8f1c
bneayoub <bne.ayoub@gmail.com>
2023-04-23 12:15:32 +0100

    debugging string handler

--- handle_string.c
+++ handle_string.c
@@ -7,17 +7,17 @@
  */
 int handle_string(va_list args)
 {
-    char *str = va_arg(args, char *);
-    int count = 0;
+	char *str = va_arg(args, char *);
+	int count = 0;
 
-    if (str == NULL)
-        str = "(null)";
+	if (str == NULL)
+		str = "(null)";
 
-    while (*str)
-    {
-        count += _putchar(*str);
-        str++;
-    }
+	while (*str)
+	{
+		count += _putchar(*str);
+		str++;
+	}
 
-    return (count);
+	return (count);
 }
