b54eaf7f9e9314f6cb022d55334af9ddc8758b79
Vhelaa1 <halabouzidi@icloud.com>
2023-04-23 12:21:50 +0000

    debugging

---
+++ test/0-main4.c
@@ -0,0 +1,12 @@
+#include "../main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_printf("Hala's IQ %s is growing.\n", (char*)0);
+	printf("Hala's IQ %s is growing.\n", (char*)0);
+	return (0);
+}
