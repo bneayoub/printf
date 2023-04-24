a77690ccb473bb7f0be76e39ff252e9e04e987c8
Vhelaa1 <halabouzidi@icloud.com>
2023-04-24 19:30:42 +0000

    more test cases

---
+++ test/0-main7.c
@@ -0,0 +1,8 @@
+#include "../main.h"
+
+int main(void)
+{
+    _printf("%s%c%c%s%%%s%s%c", "Loading", '.', '.', ".", "99", "Please wait", '\n');
+    printf("%s%c%c%s%%%s%s%c", "Loading", '.', '.', ".", "99", "Please wait", '\n');
+    return (0);
+}
