52f038fa4ae7bad2344ca8a4ba68d599d150c61f
Vhelaa1 <halabouzidi@icloud.com>
2023-04-25 09:05:12 +0000

    more test cases

--- test/0-main7.c
+++ test/0-main7.c
@@ -2,6 +2,8 @@
 
 int main(void)
 {
+    printf("-> MULTIPLE % WITH c & s SPECIFIERS <-\n");
+    printf("-------------------\n");
     _printf("%s%c%c%s%%%s%s%c", "Loading", '.', '.', ".", "99", "Please wait", '\n');
     printf("%s%c%c%s%%%s%s%c", "Loading", '.', '.', ".", "99", "Please wait", '\n');
     return (0);
