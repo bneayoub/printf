1721f03699f3eae27273327ca799d6c953144e93
bneayoub <bne.ayoub@gmail.com>
2023-04-25 12:32:05 +0100

    more test c cases

--- test/0-main2.c
+++ test/0-main2.c
@@ -6,7 +6,13 @@
  */
 int main(void)
 {
+	printf("-> SINGLE CHARS CASE <-\n");
+	printf("-------------------\n");
 	_printf("%c\n", 'H');
 	printf("%c\n", 'H');
+	printf("\n\n");
+	_printf("%c%c%c%c%c%c%c%c%c%c\n", 'H','A','L','A','-','A','Y','O','U','B');
+	printf("%c%c%c%c%c%c%c%c%c%c\n", 'H','A','L','A','-','A','Y','O','U','B');
+
 	return (0);
 }
