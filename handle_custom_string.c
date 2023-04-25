3812238a3dc7aa06407aa47644967239b11b0fb4
Vhelaa1 <halabouzidi@icloud.com>
2023-04-25 23:05:54 +0000

    updated to include new flag arg

--- handle_custom_string.c
+++ handle_custom_string.c
@@ -9,6 +9,7 @@ int handle_custom_S(va_list args)
 {
 	char *s = va_arg(args, char *);
 	int count = 0, i;
+	char flag = '\0';
 
 	if (s == NULL)
 		s = "(null)";
@@ -17,7 +18,7 @@ int handle_custom_S(va_list args)
 	{
 		if ((s[i] > 0 && s[i] < 32) || s[i] >= 127)
 		{
-			count += print_hex_char(s[i]);
+			count += print_hex_char(s[i], flag);
 		}
 		else
 		{
@@ -31,14 +32,16 @@ int handle_custom_S(va_list args)
 /**
  * print_hex_char - Prints an unsigned char as its hexadecimal value
  * @c: The character to print as hexadecimal
+ * @flag: The flag character, not used in this function, but added for consistency
  *
  * Return: The number of characters printed
  */
-int print_hex_char(unsigned char c)
+int print_hex_char(unsigned char c, char flag)
 {
 	char hex_digits[] = "0123456789ABCDEF";
 	int count = 0;
 
+	(void)flag;
 	_putchar('\\');
 	_putchar('x');
 	_putchar(hex_digits[(c >> 4) & 0xF]);
