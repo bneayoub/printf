f7dc6b027985abe436658db1c806c101ae96b46d
Vhelaa1 <halabouzidi@icloud.com>
2023-04-25 09:04:14 +0000

    more test cases

--- test/0-main8.c
+++ test/0-main8.c
@@ -6,6 +6,9 @@ int main(void)
 				"Phasellus fringilla porttitor nisi, in mollis dui sagittis eu. Quisque at libero ut justo tempus varius. Fusce aliquet, ante vitae egestas faucibus, ligula neque consectetur tortor, "
 				"vel elementum mi mauris ac enim. Suspendisse potenti. Curabitur ullamcorper fringilla luctus. Vestibulum id elit et velit scelerisque venenatis. Ut semper auctor urna, vel consequat nisi.";
 
+	printf("-> LONG STRING CASE <-\n");
+	printf("-------------------\n");
+	
 	_printf("man gcc: \n%s\n", str);
 
 	return (0);
