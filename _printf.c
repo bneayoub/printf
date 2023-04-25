999cdea8f7cd81af90c2ea4382529014a7cb2eff
bneayoub <bne.ayoub@gmail.com>
2023-04-26 00:06:15 +0100

    printf new version

--- _printf.c
+++ _printf.c
@@ -8,6 +8,7 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
+	int result;
 
 	va_start(args, format);
 	if (format == NULL)
@@ -27,7 +28,7 @@ int _printf(const char *format, ...)
 				count = -1;
 				break;
 			}
-			int result = handle_specifier(&format, args, &count);
+			result = handle_flag_and_specifier(&format, args, &count);
 
 			if (result == -1)
 			{
@@ -43,4 +44,36 @@ int _printf(const char *format, ...)
 	}
 	va_end(args);
 	return (count);
+}
+
+/**
+ * handle_flag_and_specifier - Handles flag chars and specifiers
+ * @format: Pointer to the current position in the format string
+ * @args: args list
+ * @count: Pointer to the count of characters printed so far
+ *
+ * Return: 0 on success, -1 on error
+ */
+int handle_flag_and_specifier(const char **format, va_list args, int *count)
+{
+	char flag = '\0';
+	int result;
+
+	if (**format == '+' || **format == ' ' || **format == '#')
+	{
+		flag = **format;
+		(*format)++;
+	}
+	else
+	{
+		flag = '\0';
+	}
+	result = handle_specifier(**format, args, flag);
+
+	if (result == -1)
+		return (-1);
+
+	*count += result;
+
+	return (0);
 }
