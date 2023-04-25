d7af98b712ba508b77e7eb5500ba5afcf187c4cc
Vhelaa1 <halabouzidi@icloud.com>
2023-04-25 08:57:30 +0000

    more test cases

--- test/2-main.c
+++ test/2-main.c
@@ -14,7 +14,7 @@ int main(void)
     _printf("num3 in binary: %b\n", num3);
     _printf("num4 in binary: %b\n", num4);
 	_putchar('\n');
-	_printf("num1 in binary: %b\n", num1);
+	printf("num1 in binary: %b\n", num1);
     _printf("num2 in binary: %b\n", num2);
     _printf("num3 in binary: %b\n", num3);
     _printf("num4 in binary: %b\n", num4);
