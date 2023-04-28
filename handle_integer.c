cc2038f289079b8b1bc5becaf5e76e4825f8022c
bneayoub <bne.ayoub@gmail.com>
2023-04-28 15:17:57 +0100

    updated int handler

--- handle_integer.c
+++ handle_integer.c
@@ -3,9 +3,10 @@
  * _print_integer - prints an integer using recursion
  * @n: int to print
  * @count: pointer to count of chars printed
+ * @flag: flag + or space
  * @is_start: 1 or 0 when recursion starts
  */
-void _print_integer(int n, int *count, char flag, int is_start)
+void _print_integer(long int n, int *count, char flag, int is_start)
 {
 	if (is_start)
 	{
@@ -34,12 +35,26 @@ void _print_integer(int n, int *count, char flag, int is_start)
  * handle_integer - handles the case d and i specifiers
  * @args: the argument list to get the integer from
  * @flag: flag character
+ * @length_modifier: length modifier character
  * Return: the number of chars printed
  */
-int handle_integer(va_list args, char flag)
+int handle_integer(va_list args, char flag, char length_modifier)
 {
-	int n = va_arg(args, int);
+	long int n;
 	int count = 0;
+
+	switch (length_modifier)
+	{
+	case 'l':
+		n = va_arg(args, long int);
+		break;
+	case 'h':
+		n = (short int)va_arg(args, int);
+		break;
+	default:
+		n = va_arg(args, int);
+		break;
+	}
 
 	if (n == 0)
 	{
@@ -52,11 +67,11 @@ int handle_integer(va_list args, char flag)
 		return (count + 1);
 	}
 
-	if (n == INT_MIN)
+	if (n == LONG_MIN)
 	{
 		_putchar('-');
-		_putchar('2');
-		_print_integer(147483648, &count, flag, 0);
+		_putchar('9');
+		_print_integer(223372036854775808, &count, flag, 0);
 		count += 2;
 		return (count);
 	}
